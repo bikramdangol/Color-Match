8eb88c92f5d20f6bf42b53952d3be6a6e2256afa
Bikram Dangol <bikramdangol@hotmail.com>
2016-08-13 21:20:15 -0600

    Added black border in a circular button.

--- Color Match/CircularButton.swift
+++ Color Match/CircularButton.swift
@@ -18,10 +18,23 @@ class CircularButton: UIButton {
         }
     }
     
+    @IBInspectable var borderColor: UIColor = UIColor.black {
+        didSet(oldColor) {
+            if borderColor != oldColor {
+                self.setNeedsDisplay()
+            }
+        }
+    }
+    
     override func draw(_ rect: CGRect) {
+        let minimumLength = min(bounds.width, bounds.height)
+        let borderWidth: CGFloat = minimumLength * 0.1
+        let path = UIBezierPath(arcCenter: CGPoint(x:bounds.width/2, y:bounds.height/2), radius: (minimumLength - borderWidth)/2, startAngle: 0, endAngle: 360, clockwise: true)
         
-        let path = UIBezierPath(ovalIn: rect)
+        path.lineWidth = borderWidth
+        borderColor.setStroke()
         fillColor.setFill()
+        path.stroke()
         path.fill()
         
     }

687aee1133b31607adf426b5b82b0af15344f210
Bikram Dangol <bikramdangol@hotmail.com>
2016-08-16 19:56:11 -0600

    Refactor CircularButton. + Icon size adjustment.

--- Color Match/CircularButton.swift
+++ Color Match/CircularButton.swift
@@ -9,10 +9,7 @@
 import UIKit
 
 class CircularButton: UIButton {
-    //0.559005 0.932134 1.00065 1
-    
-    //let defaultFillColor = UIColor(colorLiteralRed: 0.559005, green: 0.932134, blue: 1.00065, alpha: 1)
-    //  = UIColor(colorLiteralRed: 127/255, green: 234/255, blue: 255/255, alpha: 1)
+
     @IBInspectable var fillColor: UIColor = UIColor.clear {
         didSet(oldColor) {
             if fillColor != oldColor {
@@ -32,64 +29,61 @@ class CircularButton: UIButton {
     @IBInspectable var endColor: UIColor = UIColor.white
     
     override func draw(_ rect: CGRect) {
+        
         let minimumLength = min(bounds.width, bounds.height)
         let borderWidth: CGFloat = minimumLength * 0.05
         
         let centerX = minimumLength/2
         let centerY = minimumLength/2
         
-        let radius = minimumLength/2 - borderWidth
-        let path = UIBezierPath(arcCenter: CGPoint(x:bounds.width/2, y:bounds.height/2), radius: radius, startAngle: 0, endAngle: 360, clockwise: true)
+        let maximumPossibleRadius = minimumLength/2 - borderWidth
         
-        path.lineWidth = borderWidth
-        borderColor.setStroke()
-        //fillColor.setFill()
-        //path.stroke()
-        //path.fill()
-        
-        let context = UIGraphicsGetCurrentContext()
-        let locations: [CGFloat] = [0.0, 1.0]
-        
-        var colors = [CGColor]()
-        if fillColor == UIColor.clear{
-            colors = [UIColor.white.cgColor, UIColor.brown.cgColor]
-        } else {
-            colors = [UIColor.white.cgColor, fillColor.cgColor]
-            if borderColor == UIColor.black {
-                path.stroke()
-            }
+        if borderColor == UIColor.black {
+            // For blak border stroke
+            let path = UIBezierPath(arcCenter: CGPoint(x:bounds.width/2, y:bounds.height/2), radius: maximumPossibleRadius, startAngle: 0, endAngle: 360, clockwise: true)
+            path.lineWidth = borderWidth
+            borderColor.setStroke()
+            path.stroke()
         }
         
+        var locations: [CGFloat]
         let colorspace = CGColorSpaceCreateDeviceRGB()
-        
-        let gradient = CGGradient(colorsSpace: colorspace,
-                                  colors: colors, locations: locations)
-        
+        var colors = [CGColor]()
         var startPoint = CGPoint()
         var endPoint = CGPoint()
-        
-        if fillColor == UIColor.clear{
-            startPoint.x = centerX * 0.9
-            startPoint.y = centerY * 1.3
-            endPoint.x = centerX
-            endPoint.y = centerY
-            let startRadius: CGFloat = 0
-            let endRadius: CGFloat = radius/2
-
-            context!.drawRadialGradient (gradient!, startCenter: startPoint,
-                                         startRadius: startRadius, endCenter: endPoint, endRadius: endRadius,
-                                         options: CGGradientDrawingOptions(rawValue: UInt32(0)))
-        }else {
-            startPoint.x = centerX/1.2
-            startPoint.y = centerY/1.2
-            endPoint.x = centerX
-            endPoint.y = centerY
-            let startRadius: CGFloat = 0
-            let endRadius: CGFloat = radius * 0.75 //- borderWidth/2
-        
-            context!.drawRadialGradient (gradient!, startCenter: startPoint,
-                                     startRadius: startRadius, endCenter: endPoint, endRadius: endRadius,
-                                     options: CGGradientDrawingOptions(rawValue: UInt32(0)))
+        let context = UIGraphicsGetCurrentContext()
+        if let context = context{
+            // Make small hole
+            if fillColor == UIColor.clear{
+                startPoint.x = centerX * 0.9
+                startPoint.y = centerY * 1.3
+                endPoint.x = centerX
+                endPoint.y = centerY
+                let startRadius: CGFloat = 0
+                let endRadius: CGFloat = maximumPossibleRadius/2
+                locations = [0.0, 0.7, 1.0]
+                colors = [UIColor.lightGray.cgColor, UIColor.lightGray.cgColor, UIColor.brown.cgColor]
+                let gradient = CGGradient(colorsSpace: colorspace, colors: colors, locations: locations)
+                context.drawRadialGradient (gradient!, startCenter: startPoint,
+                                             startRadius: startRadius, endCenter: endPoint, endRadius: endRadius,
+                                             options: CGGradientDrawingOptions(rawValue: UInt32(0)))
+            }else {
+                // Normal circle
+                startPoint.x = centerX/1.2
+                startPoint.y = centerY/1.2
+                endPoint.x = centerX
+                endPoint.y = centerY
+                let startRadius: CGFloat = 0
+                let endRadius: CGFloat = maximumPossibleRadius * 0.75 //- borderWidth/2
+                locations = [0.0, 1.0]
+                colors = [UIColor.white.cgColor, fillColor.cgColor]
+                let gradient = CGGradient(colorsSpace: colorspace, colors: colors, locations: locations)
+                context.drawRadialGradient (gradient!, startCenter: startPoint,
+                                             startRadius: startRadius, endCenter: endPoint, endRadius: endRadius,
+                                             options: CGGradientDrawingOptions(rawValue: UInt32(0)))
+            }
+        } else {
+            print("No Context....")
         }
         
     }
